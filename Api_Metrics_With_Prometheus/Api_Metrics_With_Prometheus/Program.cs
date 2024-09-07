2bed1b289ed7dd9d28b166b4c7c21c07532c1fab
Geuber Lucas <geuberlu@gmail.com>
2024-09-07 10:59:43 -0300

    Update project dependencies to use  SQL SERVER

--- Api_Metrics_With_Prometheus/Api_Metrics_With_Prometheus/Program.cs
+++ Api_Metrics_With_Prometheus/Api_Metrics_With_Prometheus/Program.cs
@@ -5,13 +5,20 @@ using Microsoft.EntityFrameworkCore;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://*:80");
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString));
+    options.UseSqlServer(connectionString);
 });
 
 builder.Services.AddControllers();
@@ -23,23 +30,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOpenTelemetry().ConfigureResource(resource => resource.AddService("register people")).WithMetrics(bd =>
 {
-    
     bd.AddMeter("Microsoft.AspNetCore.Routing", "Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel");
-    
     bd.AddPrometheusExporter();
-
-    
-
 });
 
 
 var app = builder.Build();
-
 DatabaseService.InitializeDatabase(app);
 app.UseSwagger();
 app.UseSwaggerUI();
-
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
--- init-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Ensure the MSSQL data directory has the correct permissions
-chown -R mssql:mssql /var/opt/mssql
-
-# Start SQL Server
-/opt/mssql/bin/sqlservr
